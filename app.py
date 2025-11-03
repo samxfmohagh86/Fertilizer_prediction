2b121c3bf27ce85d74666a183be313d4f60d0013
samxfmohagh86 <samfxgh86@gmail.com>
2025-11-03 09:15:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def predict():
         return jsonify({'status': 'error', 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
